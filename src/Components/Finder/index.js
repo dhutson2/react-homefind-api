1d815ed80663b169c30d1ea092b0b9fbbb324094
Derek Hutson <denverd1301@gmail.com>
2019-09-03 15:03:09 -0600

    installed semantic, added form

---
+++ src/Components/Finder/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import "./finder.css";
+
+class Finder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
+  }
+}
+
+export default Finder;
