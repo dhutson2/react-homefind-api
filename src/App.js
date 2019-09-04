05ac6631fb3f33b16d96c291c5290962ae46f25b
Derek Hutson <denverd1301@gmail.com>
2019-09-04 14:56:41 -0600

    added route to fetch data from API! yay!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Finder from "./Components/Finder/index";
 import ResultsList from "./Components/ResultList/index";
 
-function App() {
-  return (
-    <div>
-      <Finder />
-      <ResultsList />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      houses: [],
+      priceRange: []
+    };
+  }
+
+  getHouses = async () => {
+    try {
+      const houses = await fetch(
+        "https://api.bridgedataoutput.com/api/v2/OData/test/Property?access_token=f947e577f8d86995ab127ed14e2ebcb4"
+      );
+      const housesJson = await houses.json();
+      return housesJson;
+    } catch (err) {
+      console.log(err, "getHouses error");
+      return err;
+    }
+  };
+
+  getPriceRange = async () => {
+    try {
+      const prices = await fetch(
+        "https://api.bridgedataoutput.com/api/v2/OData/test/Property?access_token=f947e577f8d86995ab127ed14e2ebcb4&$filter=ListPrice%20gt%20900000"
+      );
+      const pricesJson = await prices.json();
+      return pricesJson;
+    } catch (err) {
+      console.log(err, "getPriceRange error");
+      return err;
+    }
+  };
+
+  componentDidMount() {
+    this.getHouses().then(houseData => {
+      console.log("Houses data: ", houseData);
+      this.setState({
+        houses: houseData
+      });
+    });
+    this.getPriceRange().then(priceData => {
+      console.log("Price data:", priceData);
+      this.setState({
+        priceRange: priceData
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Finder />
+        <ResultsList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/ResultList/index.js
+++ src/Components/ResultList/index.js
@@ -4,35 +4,35 @@ import { List, Image } from "semantic-ui-react";
 const ResultsList = () => (
   <List>
     <List.Item>
-      <Image avatar src="/images/avatar/small/rachel.png" />
+      {/* <Image avatar src="/images/avatar/small/rachel.png" /> */}
       <List.Content>
         <List.Header as="a">Rachel</List.Header>
         <List.Description>Last seen just now.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src="/images/avatar/small/lindsay.png" />
+      {/* <Image avatar src="/images/avatar/small/lindsay.png" /> */}
       <List.Content>
         <List.Header as="a">Lindsay</List.Header>
         <List.Description>Last seen 10 hours ago.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src="/images/avatar/small/matthew.png" />
+      {/* <Image avatar src="/images/avatar/small/matthew.png" /> */}
       <List.Content>
         <List.Header as="a">Matthew</List.Header>
         <List.Description>Last seen yesterday.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src="/images/avatar/small/jenny.jpg" />
+      {/* <Image avatar src="/images/avatar/small/jenny.jpg" /> */}
       <List.Content>
         <List.Header as="a">Jenny Hess</List.Header>
         <List.Description>Last seen 3 days ago.</List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
-      <Image avatar src="/images/avatar/small/veronika.jpg" />
+      {/* <Image avatar src="/images/avatar/small/veronika.jpg" /> */}
       <List.Content>
         <List.Header as="a">Veronika Ossi</List.Header>
         <List.Description>Has not watched anything recently</List.Description>
